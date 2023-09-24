3d5dc0b622df2345a1ccbdf6bcc9b904f4ac7745
deninkq <gondat@abv.bg>
2023-09-24 11:27:27 +0300

    feat(recipes):create recipe model and recipes array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './recipe-list.component.html',
-    styleUrls: ['./recipe-list.component.css']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test',
+      'Test desc',
+      'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'
+    ),
+    new Recipe(
+        'A test2',
+        'Test desc2',
+        'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'
+      ),
+  ];
 
+  constructor() {}
 }
